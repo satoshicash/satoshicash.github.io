c6df55f0b0c63c0bc0078858de3922f015fc1ec3
satoshicash <128562394+satoshicash@users.noreply.github.com>
2023-05-21 23:15:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -45,3 +45,29 @@ listVideo.forEach(video =>{
   };
  };
 });
+var xValues = ["Community", "Liquidity", "Team", "Treasury", "Marketing"];
+var yValues = [60, 20, 10, 5, 5];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
+];
+
+new Chart("myChart", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "TOKENOMICS"
+    }
+  }
+});
